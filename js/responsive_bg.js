df6966ea677260ffb7e67d3bbc814db2d043630f
William Desktop <41762983+wille430@users.noreply.github.com>
2021-03-08 10:18:51 +0100

    bg för desktop + js script som ändrar bg efter fönster storlek

---
+++ js/responsive_bg.js
@@ -0,0 +1,10 @@
+function bgChanger(){
+    const body = document.querySelector("#home")
+    if (window.innerWidth > 800) {
+        body.style.backgroundImage = "url('./img/desktopbg.jpg'"
+    } else {
+        body.style.backgroundImage = "url('./img/homebg.jpg'"
+    }
+}
+
+window.onresize = bgChanger
